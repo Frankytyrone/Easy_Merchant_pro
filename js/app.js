af84b9a5f5ea53f52a9b19a2b39c605e3bffb8ba
Frankytyrone <55462821+Frankytyrone@users.noreply.github.com>
2026-02-20 12:14:23 +0000

    Create main application JavaScript logic for Easy Merchant Pro

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Main application logic for Easy Merchant Pro
+
+const app = () => {
+    console.log('Easy Merchant Pro is running');
+    // Add further application logic here
+};
+
+app();
